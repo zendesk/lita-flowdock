f5dea7eaa546139bd4ac709d1737dab99751453d
Adam Panzer <apanzerj@gmail.com>
2015-12-10 12:28:56 -0800

    Better rooming by storing parameterized_name and id

--- spec/lita/source/flowdock_source_spec.rb
+++ spec/lita/source/flowdock_source_spec.rb
@@ -1,21 +1,23 @@
 require 'spec_helper'
 
 describe Lita::FlowdockSource do
-  let(:room){ "Main" }
+  let(:room){ Lita::Room.new(room_id, {name: "Main"}) }
   let(:room_id) { "123123123" }
 
-  subject{ Lita::FlowdockSource.new(room: room) }
+  subject{ Lita::FlowdockSource.new(room: room_id) }
 
   describe "with a room" do
     it "looks up rooms" do
-      expect(Lita.redis).to receive(:get).with("flows/#{room}").and_return(room_id)
-      expect(subject.room).to eql(room_id)
+      expect(Lita.redis).to receive(:exists).with("flows_id/#{room_id}").and_return(true)
+      expect(Lita.redis).to receive(:get).with("flows_id/#{room_id}").and_return("Main")
+      expect(subject.room_object.id).to eql(room_id)
+      expect(subject.room_object.name).to eql(room.name)
     end
 
     describe "if the room doesn't exist" do
       it "defaults to the passed in room" do
-        expect(Lita.redis).to receive(:get).with("flows/#{room}").and_return(nil)
-        expect(subject.room).to eql(room)
+        expect(Lita.redis).to receive(:exists).with("flows_id/#{room_id}").and_return(true)
+        expect(subject.room).to eql(room_id)
       end
     end
   end
@@ -32,7 +34,7 @@ describe Lita::FlowdockSource do
 
 
   describe "with a message id" do
-    subject{ Lita::FlowdockSource.new(room: room, message_id: 123) }
+    subject{ Lita::FlowdockSource.new(room: room_id, message_id: 123) }
 
     it "saves the message id" do
       allow(Lita.redis).to receive(:get).with("flows/#{room}").and_return(room_id)
